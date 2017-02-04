de33fd08dce1b92bd8866481f3dbdc876cacca76
EricPoehlsen <eric@eric-poehlsen.de>
2017-02-05 00:17:45 +0100

    Adding Testcode

---
+++ main.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+
+    a = np.array([2, 3, 4, 5, 2, 2, 1, 3, 4, 5, 3, 2, 1, 3, 4, 5])
+
+    plot = plt.plot()
+    plot = plt.plot([1,2,3])
+    plot.show()
+
+
+if __name__ == "__main__":
+    main()
